fcc7b39f37f3aad51c65f40bef10903feda284ff
GrowlyX <growlyxdev@gmail.com>
2023-04-03 13:09:25 -0400

    feat: add notes for app

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -10,15 +10,36 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+/**
+ * An application to filter through and compare countries.
+ *
+ * They are mapped into regions, which are continents (obviously),
+ * and users can select countries to view their flag, stats, etc.
+ *
+ * The user is also able to search through all countries
+ * or region based countries to find a specific one.
+ *
+ * Users are also able to compare two countries and see the diffs
+ * of their specific stats, for example:
+ *
+ * United States: 15,000,000 population
+ * Canada: 16,000,000 population (diff -> 1,000,000)
+ *
+ * Users can also sort countries by their statistics and
+ * view "leaderboards" for them.
+ *
+ * @author GrowlyX
+ * @since 4/3/2023
+ */
 @Composable
 @Preview
 fun App()
 {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var text by remember { mutableStateOf("efe, World!") }
 
     MaterialTheme {
         Button(onClick = {
-            text = "Hello, Desktop!"
+            text = "dc, Desktop!"
         }) {
             Text(text)
         }
